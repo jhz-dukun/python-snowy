ff7fb3bdc031096e816a2c48ba232072ab175d7f
dukun <dukun>
2016-11-28 16:02:39 +0800

    create project

---
+++ crawler.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+引入自定义模块的两种方式
+from getSources import myPrint
+import getSources -> getSources.myPrint()
+'''
+
+print("hello, world!")
+
+
+
+
+
+
---
+++ getSources.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def myPrint() -> object:
+    print("hello, DK!")
