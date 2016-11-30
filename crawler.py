698a1b2af5875b3bd0336fad7301efee88bbfc88
dukun <dukun>
2016-11-30 18:19:13 +0800

    20161130 save

--- crawler.py
+++ crawler.py
@@ -7,7 +7,28 @@ from getSources import myPrint
 import getSources -> getSources.myPrint()
 '''
 
+from getSources import GetSource
+import re
+import requests
+import testDemo
+
+def requestTest():
+    params = {'firstname': 'Du', 'lastname' : 'Kun'}
+    r = requests.post("http://pythonscraping.com/files/processing.php",data=params)
+    print(r.text)
+
 print("hello, world!")
+
+#requestTest()
+
+url = "http://www.zhihu.com/"
+
+news = GetSource(url)
+
+client = testDemo.ZhiHuClient()
+client.login("18681821674","north456123789")
+
+
 
 
 
