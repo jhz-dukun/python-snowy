a637cd4fc6e95079c78577f486215914d3442b39
dukun <dukun>
2016-12-05 20:30:35 +0800

    add login with cookie

--- crawler.py
+++ crawler.py
@@ -19,6 +19,7 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 requests.packages.urllib3.disable_warnings(InsecurePlatformWarning)
 
 captchaFile = os.path.join(sys.path[0], "captcha.jpg")
+cookieFile = os.path.join(sys.path[0], "cookie")
 session = requests.session()
 headers = {
     'Connection': 'keep-alive',
@@ -29,13 +30,17 @@ headers = {
     'Host': 'www.zhihu.com',
 }
 
-
-
 login_data = {'phone_num': '18681821674',
              'password': 'north456123789',
              'Referer': 'https://www.zhihu.com/',
              'remember_me': 'true',
               }
+
+def zhihuOpen(url,headers,delay=0,timeout=10):
+    """打开网页，返回Response对象"""
+    if delay:
+        time.sleep(delay)
+    return session.get(url, headers = headers,timeout=timeout, verify=False)
 
 def get_captcha():
     t = str(int(time.time()*1000))
@@ -48,20 +53,59 @@ def get_captcha():
     os.remove(captchaFile)
     return captcha
 
+def loadCookie():
+    """读取cookie文件，返回反序列化后的dict对象，没有则返回None"""
+    if os.path.exists(cookieFile):
+        print("=" * 100)
+        with open(cookieFile, "r") as f:
+            cookie = json.load(f)
+            return cookie
+    return None
 
-response = session.get('http://www.zhihu.com',headers=headers,verify = False).text
-print(response)
-soup = bs(response,"html.parser")
-xsrf = soup.find('input',{'name':'_xsrf'})['value']
-print(xsrf)
-login_data['_xsrf'] = xsrf
-login_data['captcha'] = get_captcha()
-responed = session.post('https://www.zhihu.com/login/phone_num',headers=headers,data=login_data)
+def saveCookie():
+    """cookies 序列化到文件
+    即把dict对象转化成字符串保存
+    """
+    #global session
+    with open(cookieFile, "w") as output:
+        cookies = session.cookies.get_dict()
+        json.dump(cookies, output)
+        print("=" * 100)
+        print("已在同目录下生成cookie文件：", cookieFile)
+
+cookie = loadCookie()
+
+if cookie:
+    print("检测到cookie文件，直接使用cookie登录")
+    session.cookies.update(cookie)
+    soup = bs(zhihuOpen(r"http://www.zhihu.com/",headers).text, "html.parser")
+    print("已登陆账号： %s" % soup.find("span", class_="name").getText())
+else:
+    print("没有找到cookie文件，将调用login方法登录一次！")
+    zhihuLogin()
+
+def zhihuLogin():
+    response = session.get('http://www.zhihu.com', headers=headers, verify=False).text
+    print(response)
+    soup = bs(response, "html.parser")
+    xsrf = soup.find('input', {'name': '_xsrf'})['value']
+    print(xsrf)
+    login_data['_xsrf'] = xsrf
+    login_data['captcha'] = get_captcha()
+    responed = session.post('https://www.zhihu.com/login/phone_num', headers=headers, data=login_data)
+    if 0 == responed.json()['r']:
+        saveCookie()
+        print(responed.text)
+    else:
+        print("login failed!")
+
+
+
 #print(responed)
-print(responed.json()['r'])
-print(responed.text)
 
-startData = session.get("https://www.zhihu.com",headers = headers)
+
+startData = zhihuOpen("https://www.zhihu.com",headers)
 print(startData.text)
 with open("zhihu.html","w",encoding='utf-8') as f:
     f.write(startData.text)
+    f.close()
